7d0de7300962c5f6cf92fd968fae7d0b48309796
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-06-11 16:58:29 +0900

    implement dist monad examples

---
+++ src/mp_in_clojure/algo/dist.clj
@@ -0,0 +1,21 @@
+(ns mp-in-clojure.algo.dist
+  (:require [clojure.algo.monads :as m]))
+
+(defn dist [& kvs]
+  (apply hash-map kvs))
+
+(defn uniform [s]
+  (let [n (count s)]
+    (->> s
+         (map (fn [x] [x (/ 1 n)]))
+         (into {}))))
+
+(m/defmonad dist-m
+  [m-result (fn [v] {v 1})
+   m-bind   (fn [mv f]
+              (letfn [(add-prob [dist [x p]]
+                        (assoc dist x (+ (get dist x 0) p)))]
+                (reduce add-prob {}
+                        (for [[x p] mv
+                              [y q] (f x)]
+                          [y (* q p)]))))])
