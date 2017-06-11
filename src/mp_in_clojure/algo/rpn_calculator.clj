db57b9b0d5f95bdb37d49b15f3467386ec2fde61
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-06-11 16:39:54 +0900

    implement RPN calculator

---
+++ src/mp_in_clojure/algo/rpn_calculator.clj
@@ -0,0 +1,43 @@
+(ns mp-in-clojure.algo.rpn-calculator
+  (:require [clojure.string :as str]
+            [clojure.algo.monads :as m]))
+
+;;; without monads
+
+(defn- folding-function [[x y & ys :as xs] s]
+  (cond
+    (and x y (= s "*")) (conj ys (* y x))
+    (and x y (= s "+")) (conj ys (+ y x))
+    (and x y (= s "-")) (conj ys (- y x))
+    :else (conj xs (Double/parseDouble s))))
+
+(defn solve-rpn [s]
+  (as-> s v
+    (str/split v #"\s+")
+    (reduce folding-function () v)
+    (first v)))
+
+;;; with maybe monad
+
+(defn- read-maybe [s]
+  (try
+    (Double/parseDouble s)
+    (catch NumberFormatException _
+      nil)))
+
+(defn folding-with-maybe [[x y & ys :as xs] s]
+  (cond
+    (and x y (= s "*")) (conj ys (* y x))
+    (and x y (= s "+")) (conj ys (+ y x))
+    (and x y (= s "-")) (conj ys (- y x))
+    :else (m/with-monad m/maybe-m
+            ((m/m-lift 1 #(conj xs %))
+             (read-maybe s)))))
+
+(defn solve-rpn' [folding-function s]
+  (m/domonad m/maybe-m
+    [result (reduce folding-function
+                    ()
+                    (str/split s #"\s+"))
+     :when (= (count result) 1)]
+    (first result)))
