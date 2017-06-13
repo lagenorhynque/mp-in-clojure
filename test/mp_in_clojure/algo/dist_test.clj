b627ed7adf89ce4155f23490aae597750190c0bc
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-06-13 17:56:43 +0900

    throwing dice example with algo.monads

--- test/mp_in_clojure/algo/dist_test.clj
+++ test/mp_in_clojure/algo/dist_test.clj
@@ -3,6 +3,18 @@
             [mp-in-clojure.algo.dist :refer :all]
             [clojure.algo.monads :as m]))
 
+;;; throwing dice
+
+(t/deftest throwing-dice
+  (let [die (range 1 (inc 6))
+        dist (prob->dist
+              (m/domonad prob-m
+                [d1 (uniform die)
+                 d2 (uniform die)]
+                (+ d1 d2)))]
+    (t/is (= dist {2 1/36, 3 2/36, 4 3/36, 5 4/36, 6 5/36, 7 6/36,
+                   8 5/36, 9 4/36, 10 3/36, 11 2/36, 12 1/36}))))
+
 ;;; Monty Hall problem
 
 (t/deftest monty-hall-problem
